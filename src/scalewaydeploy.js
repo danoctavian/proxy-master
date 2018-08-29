88963e20294f615a6c4d196b3a0095b8f3beb344
Dan Octavian <danoctavian91@gmail.com>
2018-08-30 01:29:26 +0300

    more unfunctional bullshit

--- src/scalewaydeploy.js
+++ src/scalewaydeploy.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const fs = require('fs')
-node_ssh = require('node-ssh')
+SSHClient = require('node-ssh')
 const log = require('./logging')
 
 const PROXY_MASTER_NAME = 'proxy-master'
@@ -8,13 +8,21 @@ const PROXY_WORKER_NAME = 'proxy-worker'
 const UBUNTU_XENIAL_IMAGE = '67375eb1-f14d-4f02-bb42-6119cecbde51'
 
 const setupScript = `
-sudo apt-get install git curl python-software-properties ;
+sudo apt-get -y install git curl python-software-properties ;
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - ;
-sudo apt-get install nodejs ;
+sudo apt-get -y install nodejs ;
 rm -rf proxy-master ;
 git clone https://github.com/danoctavian/proxy-master.git ;
 cd proxy-master ;
 `
+
+const BASE_URL = 'https://cp-par1.scaleway.com'
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
 
 async function deploy(config) {
 
@@ -32,7 +40,7 @@ async function deploy(config) {
   const orgs = orgsRes.data.organizations
   log.info(`Using org with id ${orgs[0].id}`)
 
-  const serverRes = await axios.get('https://cp-par1.scaleway.com/servers', conf)
+  const serverRes = await axios.get(`${BASE_URL}/servers`, conf)
   const servers = serverRes.data.servers
 
   const masterServers = servers.filter(s => s.name.startsWith(PROXY_MASTER_NAME))
@@ -63,21 +71,40 @@ async function deploy(config) {
         "boot_type": "local"
       }
 
-      const newServer = await axios.post('https://cp-par1.scaleway.com/servers', serverConf, conf)
-      masterServer = newServer
+      let newServerRes = await axios.post(`${BASE_URL}/servers`, serverConf, conf)
+      let newServer = newServerRes.data
+
+
+
+      const powerOnRes = await axios.post(`${BASE_URL}/servers/${newServer.server.id}/action`, { action: 'poweron' }, conf)
+
+      log.info(`Triggered power on for ${newServer.server.id}. polling..`)
+
+      while (newServer.server.state !== 'running') {
+        await sleep(1000)
+        newServerRes = await axios.get(`${BASE_URL}/servers/${newServer.server.id}`, conf)
+        newServer = newServerRes.data
+      }
+
+      log.info(`Server ${newServer.server.id} operational at public IP ${newServer.server.public_ip.address}. waiting for a bit..`)
+
+      await sleep(3000)
+      masterServer = newServer.server
     } else {
       masterServer = masterServers[0]
     }
 
-    const ssh = new node_ssh()
+    const ssh = new SSHClient()
     await ssh.connect({
       host: masterServer.public_ip.address,
       username: 'root',
       privateKey: config.sshKey
     })
 
-    log.info(`SSH conn to ${masterServer.public_ip.address} ready.`)
-    const result = await ssh.exec(setupScript)
+    log.info(`SSH conn to ${masterServer.public_ip.address} ready. deploying setup script and then running it.`)
+    await ssh.putFiles([{ local: './setup.sh', remote: '/root/setup.sh'}])
+
+    const result = await ssh.exec('bash ./setup.sh')
     console.log(result)
   }
 }

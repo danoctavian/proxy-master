7e7aac31c601015a7d2c7c449dfbaed8a4dba4b9
Dan Octavian <danoctavian91@gmail.com>
2018-10-16 01:44:23 +0300

    update deploy procedure

--- rundeploy.js
+++ rundeploy.js
@@ -3,7 +3,14 @@ const log = require('./src/logging')
 
 ;(async () => {
   const deployer = new ScalewayDeployer()
-  await deployer.deploy({master: {}, sshKey: '/Users/dan/.ssh/id_rsa', worker: { desiredCount: 9}})
+  await deployer.deploy({
+    master: {},
+    sshKey: '/Users/dan/.ssh/id_rsa',
+    worker: {
+      desiredCount: 9
+    },
+    versionConstraint: '>= 1.0.0'
+  })
 })().catch(e => {
   log.error(`Deploy error: ${e.stack}`)
   process.exit(1)
--- src/scalewaydeploy.js
+++ src/scalewaydeploy.js
@@ -64,11 +64,72 @@ class ScalewayDeployer {
 
     if (config.master) {
       await this.deployMaster(masterServers)
+      await this.updateServers(masterServers, config.versionConstraint)
     }
 
     if (config.worker) {
       await this.deployWorkers(workerServers, config.worker)
+      await this.updateServers(workerServers, config.versionConstraint)
     }
+  }
+
+  async updateServers(servers, versionConstraint) {
+    if (!versionConstraint) {
+      log.info(`No version constraint specified. No updates will be performed.`)
+      return
+    }
+
+    log.info(`Checking for necessary updates on ${servers.length} servers with version constraint ${versionConstraint}`)
+
+    const serversToBeUpdated = []
+    for (let i = 0; i < servers.length; i++) {
+      const server = servers[i]
+      const ip = server.public_ip.address
+      log.info(`Checking version for server ${servers[0].name} at ip ${ip}`)
+
+      let versionResponse = null
+      try {
+        versionResponse = await axios.get(`http://${ip}:2000/version`)
+      } catch (e) {
+        if (e.message.includes('404') || e.message.includes('ECONNREFUSED')) {
+          log.warn(`server ${server.name} does not have endpoint /version. skipping`)
+        } else {
+          log.error(`Failed to query server ${server.name} with ${e.stack}`)
+        }
+        continue
+      }
+
+      const version = versionResponse.body.version
+      const satisfies = semver.satisfies(version, versionConstraint)
+      log.info(`Version for ${servers[0].name} is ${version} and satisfies ${versionConstraint} : ${satisfies}`)
+      serversToBeUpdated.append(server)
+    }
+
+    log.info(`Number of servers that need updates: ${serversToBeUpdated.length}. Starting updates concurrently.`)
+
+    await Promise.all(serversToBeUpdated.map(async server =>  {
+      try {
+        await this.applySoftwareUpdate(server)
+      } catch (e) {
+        log.error(`Failed to update server ${server.name}`)
+      }
+    }))
+  }
+
+  async applySoftwareUpdate(server) {
+    log.info(`Updating server ${server.name}`)
+    const ssh = new SSHClient()
+    await ssh.connect({
+      host: server.public_ip.address,
+      username: 'root',
+      privateKey: config.sshKey
+    })
+
+    log.info(`SSH conn to ${server.public_ip.address} ready. deploying setup script and then running it.`)
+    await ssh.putFiles([{ local: './setup.sh', remote: '/root/setup.sh'}])
+
+    const result = await ssh.exec('bash ./setup.sh')
+    console.log(result)
   }
 
   async deployMaster(masterServers) {
@@ -83,19 +144,6 @@ class ScalewayDeployer {
       log.info(`Master already present.`)
       masterServer = masterServers[0]
     }
-
-    // const ssh = new SSHClient()
-    // await ssh.connect({
-    //   host: masterServer.public_ip.address,
-    //   username: 'root',
-    //   privateKey: config.sshKey
-    // })
-    //
-    // log.info(`SSH conn to ${masterServer.public_ip.address} ready. deploying setup script and then running it.`)
-    // await ssh.putFiles([{ local: './setup.sh', remote: '/root/setup.sh'}])
-    //
-    // const result = await ssh.exec('bash ./setup.sh')
-    // console.log(result)
   }
 
   async deployWorkers(workerServers, config) {
@@ -126,7 +174,6 @@ class ScalewayDeployer {
     } else {
         log.error(`There are more alive workers (${workerServers.length}) than desiredCount (${config.desiredCount}). Kill some manually.`)
     }
-
   }
 
   async setupNewVPS(baseName) {
@@ -145,7 +192,6 @@ class ScalewayDeployer {
 
     let newServerRes = await axios.post(`${BASE_URL}/servers`, serverConf, this.conf)
     let newServer = newServerRes.data
-
 
     const powerOnRes = await axios.post(`${BASE_URL}/servers/${newServer.server.id}/action`, { action: 'poweron' }, this.conf)
 

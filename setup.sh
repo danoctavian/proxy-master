18d3caf070aa16dc28dcbb83e118070562d9f3f0
Dan Octavian <danoctavian91@gmail.com>
2018-10-18 02:16:06 +0300

    add node version checking first

--- setup.sh
+++ setup.sh
@@ -3,8 +3,17 @@ sudo apt-get install dialog apt-utils -y;
 sudo apt-get -y install git ;
 sudo apt-get -y install curl ;
 sudo apt-get -y install python-software-properties ;
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - ;
-sudo apt-get -y install nodejs ;
+
+NODEVERSION="$(node --version)"
+echo $NODEVERSION
+if [[ $NODEVERSION == v8* ]]; then
+  echo "node is already at correct version. no action required."
+else
+  echo "node nonexistant or wrong version. installing."
+  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - ;
+  sudo apt-get -y install nodejs ;
+fi
+
 npm install -g pm2;
 if [ -d "proxy-master" ]; then
   echo "proxy-master directory already exists. git pull only will be executed.";
